fb0be6755b925f4e745b4a3a8cddfc481374ccb0
Daniil <69499105+DaniilTR@users.noreply.github.com>
2026-03-17 20:12:45 +0500

    изменил редми

--- app/src/main/java/ru/uni/mdnotes/ui/NotesListScreen.kt
+++ app/src/main/java/ru/uni/mdnotes/ui/NotesListScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -62,8 +63,7 @@ fun NotesListScreen(
                     }
                 }
             )
-        }
-        ,
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = onCreateNew) {
                 Text("+")
